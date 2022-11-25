9c7cc00ab4c3407201dcdf97dc580f26478fa47b
wildlife_pc <wildlife@example.com>
2022-11-25 13:50:46 +0900

    add sample script

---
+++ exec_sample.sh
@@ -0,0 +1,4 @@
+python exec_clip.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/GIFU_WildBoar/R3_Kinkazan_Boar_REST output_dir=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/GIFU_WildBoar/R3_Kinkazan_Boar_REST-clip
+python exec_mdet.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples
+python exec_mdetcrop.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples mdet_result_path=_test_dataset/R3_Kinkazan_REST_Boar_Samples/detector_output.json
+python exec_cls.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples-crop
