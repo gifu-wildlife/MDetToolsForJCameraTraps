5062668dffdb7f54d249da67edd526c06dd99b50
wildlife_pc <wildlife@example.com>
2023-02-27 15:04:46 +0900

    bug fix

--- exec_sample.sh
+++ exec_sample.sh
@@ -2,3 +2,4 @@ python exec_clip.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_
 python exec_mdet.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples
 python exec_mdetcrop.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples mdet_result_path=_test_dataset/R3_Kinkazan_REST_Boar_Samples/detector_output.json
 python exec_cls.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples-crop
+python exec_imgsummary.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dat
