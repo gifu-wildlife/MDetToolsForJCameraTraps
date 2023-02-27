2bad21b2162f0aaa0525768bc0b6cf65de51a998
wildlife_pc <wildlife@example.com>
2023-02-27 16:35:59 +0900

    MegaDetector v5

--- download_md_model.sh
+++ download_md_model.sh
@@ -1,3 +1,3 @@
-# wget -P models/ https://github.com/microsoft/CameraTraps/releases/download/v5.0/md_v5a.0.0.pt
-# wget -P models/ https://github.com/microsoft/CameraTraps/releases/download/v5.0/md_v5b.0.0.pt
+wget -P models/ https://github.com/microsoft/CameraTraps/releases/download/v5.0/md_v5a.0.0.pt
+wget -P models/ https://github.com/microsoft/CameraTraps/releases/download/v5.0/md_v5b.0.0.pt
 wget -P models/ https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/md_v4.1.0/md_v4.1.0.pb
--- exec_sample.sh
+++ exec_sample.sh
@@ -1,5 +1,15 @@
-python exec_clip.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/GIFU_WildBoar/R3_Kinkazan_Boar_REST output_dir=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/GIFU_WildBoar/R3_Kinkazan_Boar_REST-clip
-python exec_mdet.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples
-python exec_mdetcrop.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples mdet_result_path=_test_dataset/R3_Kinkazan_REST_Boar_Samples/detector_output.json
-python exec_cls.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dataset/R3_Kinkazan_REST_Boar_Samples-crop
-python exec_imgsummary.py session_root=/home/dl-node01/MDetToolsForJCameraTraps/_test_dat
+# mkdir ~/git
+# cd ~/git
+# git clone https://github.com/ecologize/yolov5/
+# git clone https://github.com/Microsoft/cameratraps
+# git clone https://github.com/Microsoft/ai4eutils
+# export PYTHONPATH="$PYTHONPATH:$HOME/git/ai4eutils:$HOME/git/yolov5"
+
+cd ~/MDetToolsForJCameraTraps
+video_dir="/home/data_ssd/TEST-R3_Kinkazan_Boar_REST"
+
+# python exec_clip.py session_root=${video_dir} output_dir=${video_dir}-clip
+python exec_mdet.py session_root=${video_dir}-clip mdet_config.model_path=./models/md_v5a.0.0.pt
+# python exec_mdetcrop.py session_root=${video_dir}-clip mdet_result_path=${video_dir}-clip/detector_output.json
+# python exec_cls.py session_root=${video_dir}-clip-crop
+# python exec_imgsummary.py session_root=${video_dir}-clip-crop mdet_result_path=${video_dir}-clip/detector_output.json
