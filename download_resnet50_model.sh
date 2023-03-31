b42ebfbbf39c5aaec1515f1fbf5db6fc43146ec5
Masaki-Ando <ando.masaki.i7@f.gifu-u.ac.jp>
2023-03-31 10:17:19 +0900

    Fixed some bugs

--- download_resnet50_model.sh
+++ download_resnet50_model.sh
@@ -1,2 +1,5 @@
-wget -P models/classifire/ https://drive.google.com/file/d/1deKYwmQz4oUlb3wVw-wbmZDzBbdMTZ79/view?usp=sharing
-wget -P models/classifire/ https://drive.google.com/file/d/1YlNDUAQ4xpUIZ0BV4HtOqAnxEiFCy7iZ/view?usp=share_link
+wget "https://drive.google.com/uc?export=download&id=1deKYwmQz4oUlb3wVw-wbmZDzBbdMTZ79" -O models/classifire/category.txt
+curl -sc cookie "https://drive.google.com/uc?export=download&id=1YlNDUAQ4xpUIZ0BV4HtOqAnxEiFCy7iZ" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' cookie)"
+curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1YlNDUAQ4xpUIZ0BV4HtOqAnxEiFCy7iZ" -o models/classifire/15cat_50epoch_resnet50.pth
+rm cookie
