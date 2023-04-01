b3611a1d8e21a3d823b38087ee06b508fbc5f3eb
Menrui <cas_toru@icloud.com>
2023-04-01 10:57:40 +0900

    update exec_sample and README.md

--- exec_sample.sh
+++ exec_sample.sh
@@ -7,6 +7,7 @@
 # git clone https://github.com/Microsoft/ai4eutils
 # export PYTHONPATH="$PYTHONPATH:$HOME/git/ai4eutils:$HOME/git/yolov5"
 
-###Run Megadetector v5.0 
-
+### Run Megadetector v5.0 
+# cd ~/MDetToolsForJCameraTraps
+# wget -P models/ https://github.com/microsoft/CameraTraps/releases/download/v5.0/md_v5a.0.0.pt
 # python exec_mdet.py session_root=${video_dir}-clip mdet_config.model_path=./models/md_v5a.0.0.pt
